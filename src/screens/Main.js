6dea908b7843fa1479173965f877f14ee356a4fe
Wellington Cunha <wellington@programer.com.br>
2022-07-02 23:54:08 +0400

    location lib successful implemented

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import type {Node} from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
+import GetLocation from '@react-native-community/geolocation';
 
 const Main: () => Node = () => {
+  GetLocation.setRNConfiguration({
+    enableHighAccuracy: true,
+    timeout: 15000,
+  });
+  GetLocation.getCurrentPosition(
+    data => {
+      console.log(data);
+    },
+    error => console.log(error),
+  );
 
   return (
     <SafeAreaView>

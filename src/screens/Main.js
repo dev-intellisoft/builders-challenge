aa013bdf5b8a4d6e4d049d30929ef777ec012f95
Wellington Cunha <wellington@programer.com.br>
2022-07-02 18:31:07 +0400

    savepoint

---
+++ src/screens/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import type {Node} from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const Main: () => Node = () => {
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+export default Main;
